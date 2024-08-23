58fbb484d726d99dd02a6a4c21d8f578de927864
f1-：） <surprise_a@163.com>
2024-08-23 19:03:36 +0800

    完善Button組件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Button onClick={() => alert(1)}>按钮</Button> */}
+        <Button
+          className="custom"
+          onClick={() => {
+            alert("123");
+          }}
+          btnType={ButtonType.Primary}
+        >
+          Primary
+        </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>
+          Large Primary
+        </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Large Primary
+        </Button>
+        <Button btnType={ButtonType.Danger} href="www.baidu.com">
+          Danger
+        </Button>
+        <Button disabled>Disabled</Button>
+        <Button btnType={ButtonType.Link} href="www.baidu.com">
+          Link
+        </Button>
+
+        <Button btnType={ButtonType.Link} disabled href="www.baidu.com">
+          Disabled Link
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
