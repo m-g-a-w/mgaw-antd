671e32a99307814d092492b86ec52c228d99cc1b
f1-：） <surprise_a@163.com>
2024-08-23 15:17:31 +0800

    初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +17,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
