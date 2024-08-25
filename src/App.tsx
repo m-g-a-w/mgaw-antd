4a8007ed05f93fe6d7d49110d83e4c21b7158fc8
f1-：） <surprise_a@163.com>
2024-08-25 18:52:03 +0800

    Menu组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+// Button
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+// Menu
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
+import SubMenu from "./components/Menu/SubMenu";
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        {/* Button */}
         {/* <Button onClick={() => alert(1)}>按钮</Button> */}
         <Button
           className="custom"
@@ -27,21 +33,41 @@ const App: React.FC = () => {
         <Button btnType={ButtonType.Link} href="www.baidu.com">
           Link
         </Button>
-
         <Button btnType={ButtonType.Link} disabled href="www.baidu.com">
           Disabled Link
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Menu */}
+        <div>
+          {/* 横向菜单 */}
+          <Menu
+            defaultIndex="0"
+            onSelect={(index: string) => {
+              alert(index);
+            }}
+          >
+            <MenuItem>dropdown1</MenuItem>
+            <MenuItem disabled>dropdown2</MenuItem>
+            <SubMenu title="dropdown3">
+              <MenuItem>ccc</MenuItem>
+              <MenuItem>ddd</MenuItem>
+            </SubMenu>
+          </Menu>
+          {/* 纵向菜单 */}
+          <Menu
+            mode="vertical"
+            onSelect={(index: string) => {
+              alert(index);
+            }}
+            defaultOpenSubMenus={["2"]}
+          >
+            <MenuItem>dropdown1</MenuItem>
+            <MenuItem disabled>dropdown2</MenuItem>
+            <SubMenu title="dropdown3">
+              <MenuItem>ccc</MenuItem>
+              <MenuItem>ddd</MenuItem>
+            </SubMenu>
+          </Menu>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/Menu/SubMenu.tsx
@@ -0,0 +1,94 @@
+import React, { useState, useContext, FunctionComponentElement } from "react";
+import classnames from "classnames";
+import { MenuItemProps } from "./menuItem";
+import { MenuContext } from "./menu";
+
+export interface SubMenuProps {
+  index?: string;
+  title: string;
+  className?: string;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+const SubMenu: React.FC<SubMenuProps> = ({
+  index,
+  style,
+  title,
+  className,
+  children,
+}) => {
+  const context = useContext(MenuContext);
+  const opendSubMenus = context.defaultOpenSubMenus as Array<String>;
+  const isOpened =
+    index && context.mode === "vertical"
+      ? opendSubMenus.includes(index)
+      : false;
+  const [menuOpen, setOpen] = useState(isOpened);
+  const classes = classnames("menu-item submenu-item", className, {
+    "is-active": context.index === index,
+  });
+  //点击事件
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setOpen(!menuOpen);
+  };
+  let timer: any;
+  const handleMouse = (e: React.MouseEvent, toggle: boolean) => {
+    clearTimeout(timer);
+    e.preventDefault();
+    timer = setTimeout(() => {
+      setOpen(toggle);
+    }, 300);
+  };
+  const clickEvents =
+    context.mode === "vertical"
+      ? {
+          onClick: handleClick,
+        }
+      : {};
+  const hoverEvents =
+    context.mode !== "vertical"
+      ? {
+          onMouseEnter: (e: React.MouseEvent) => handleMouse(e, true),
+          onMouseLeave: (e: React.MouseEvent) => handleMouse(e, false),
+        }
+      : {};
+  //   渲染下拉菜单内容
+  const renderChildren = () => {
+    const subMenuClasses = classnames("mgaw-submenu", {
+      "menu-opened": menuOpen,
+    });
+    const childrenComponent = React.Children.map(children, (child, i) => {
+      const childElement = child as FunctionComponentElement<MenuItemProps>;
+      if (childElement.type.displayName === "MenuItem") {
+        return React.cloneElement(childElement, {
+          index: `${index}-${i}`,
+        });
+      } else {
+        console.error(
+          "Warning: SubMenu has a child which is not a MenuItem component"
+        );
+      }
+    });
+    return <ul className={subMenuClasses}>{childrenComponent}</ul>;
+  };
+  return (
+    <li
+      key={index}
+      className={classes}
+      style={style}
+      {...hoverEvents}
+      onClick={handleClick}
+    >
+      <div className="submenu-title" {...clickEvents}>
+        {title}
+      </div>
+      {renderChildren()}
+    </li>
+  );
+};
+
+SubMenu.displayName = "SubMenu";
+
+export default SubMenu;
